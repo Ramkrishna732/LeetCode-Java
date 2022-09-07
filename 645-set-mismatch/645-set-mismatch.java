a83649b23de2dcda99284236324e1ac0ccdbf4ee
Ramkrishna Maurya <68507596+Ramkrishna732@users.noreply.github.com>
2022-09-07 19:57:30 +0530

    Time: 581 ms (5.00%), Space: 54.3 MB (74.05%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] findErrorNums(int[] nums) {
+        
+        
+        int duplicate = -1, missing = -1;
+        for (int i = 1; i <= nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i)
+                    count++;
+            }
+            if (count == 2)
+                duplicate = i;
+            else if (count == 0)
+                missing = i;
+            if (duplicate > 0 && missing > 0)
+                break;
+        }
+        return new int[] {duplicate, missing};
+    }
+}
